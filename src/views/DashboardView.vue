0a8ae167f124cc9a0ce42156facc09e256dc9947
JulioSalazar12 <jsalazarzapata2@gmail.com>
2023-11-19 18:59:38 -0500

    cambios de plans

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,7 +4,7 @@
     <p>Bienvenido al panel de control</p>
 
     <v-card width="1000" class="pa-5 mt-5" outlined>
-    <v-form @submit.prevent="submitLoanForm" class="d-flex flex-column">
+    <v-form v-if="showForm" @submit.prevent="submitLoanForm" class="d-flex flex-column">
       <!-- Primera columna -->
       <v-row>
         <v-col cols="12" md="6">
@@ -39,18 +39,36 @@
         </v-col>
 
       </v-row>
-      <v-row>
+      <v-row class="pt-0 mt-0">
         <v-col cols="12" md="12">
           <v-btn class="elevation-0" type="submit" color="primary">Submit</v-btn>
+          <v-btn class="elevation-0 ml-3" color="success" @click="showForm = !showForm">Close Form</v-btn>
         </v-col>
       </v-row>
       
     </v-form>
+    <v-btn class="elevation-0" v-if="!showForm" color="success" @click="showForm = !showForm">View Form</v-btn>
   </v-card>
+  
+  <div class="d-flex justify-space-between mt-8" style="width: 80%;">
+    <span class="mt-3 font-weight-black">ITEMS:</span>
+    <v-btn class="elevation-0" color="indigo" dark>Refresh</v-btn>
+  </div>
+
+  <div v-if="itemsList!=[]" class="d-flex mt-3 flex-wrap justify-center" style="gap: 10px;">
+    <v-card v-for="(item, index) in itemsList" :key="index" outlined class="pa-4 d-flex flex-column align-start" width="350">
+      <h3>SAMPLE ID: 000-{{ item.id }}</h3>
+      <p class="mb-0">sellPrice: {{item.sellPrice}}</p>
+      <p class="mb-0">paymentPlan: {{item.paymentPlan}}</p>
+      <p class="mb-0">interestType: {{item.interestType}}</p>
+      <p class="mb-0">capitalizationPeriod: {{item.capitalizationPeriod}}</p>
+    </v-card>
+
+  </div>
 
   <v-snackbar v-model="snackbar.show" :color="snackbar.color" timeout="3000">
-            {{ snackbar.message }}
-        </v-snackbar>
+      {{ snackbar.message }}
+  </v-snackbar>
 
 
   </div>
@@ -60,6 +78,8 @@
 export default {
   data() {
     return {
+      showForm: true,
+
       snackbar: {
         show: false,
         color: '', // Puedes personalizar el color según el tipo de mensaje (éxito, error, etc.)
@@ -96,6 +116,9 @@ export default {
       interestTypOptions: ['TEA', 'TNA'],
       capitalizatOptions: ['DIARIA', 'MENSUAL'],
       // Agrega más opciones según sea necesario
+
+
+      itemsList: [],
     };
   },
   created(){
@@ -155,6 +178,23 @@ export default {
           // Muestra un mensaje de error
           this.showSnackbar('Error al enviar la solicitud', 'error');
         });
+      },
+      getAllItems(){
+        this.$axios.get(`/plans/users/${this.idUser}`)
+          .then(response => {
+            // Maneja la respuesta del servidor después de la solicitud POST
+            console.log('Respuesta del servidor:', response.data);
+            this.itemsList = response.data;
+            // Muestra un mensaje de éxito
+            this.showSnackbar('Lista de items actualizada con éxito', 'success');
+          })
+          .catch(error => {
+            // Maneja el error después de la solicitud POST
+            console.error('Error en la solicitud:', error);
+            // Muestra un mensaje de error
+            this.showSnackbar('Lista de items error', 'error');
+          });
+        
     },
     showSnackbar(message, color) {
         this.snackbar.show = true;
